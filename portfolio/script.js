df765b545f62a83dbc278495fcd5f0de2e976fc6
amanseth10 <90022190+amanseth10@users.noreply.github.com>
2023-09-15 09:36:16 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["am a FrontEnd Developer","love Coding","am a Learner","love Web Designing"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+        fadeOut: true,
+        
+    });
+    var typed = new Typed(".typing-2",{
+        strings: ["am a FrontEnd Developer","love Coding","am a Learner","love Web Designing"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+});
+
